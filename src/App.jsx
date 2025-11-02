f80bd584c9ea57d6c0b06f0a2fcfa808a501dd4a
DebFiore <deb@mergemedia.ai>
2025-11-01 18:33:01 -0700

    Add SchemaManager for automatic schema loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import Pages from "@/pages/index.jsx"
-import { Toaster } from "@/components/ui/toaster"
+import Pages from '@/pages/Index.jsx'
+import { Toaster } from '@/components/ui/toaster'
+import { SchemaManager } from './components/Schema'
 
 function App() {
   return (
     <>
+      <SchemaManager pageType="homepage" />
       <Pages />
       <Toaster />
     </>
   )
 }
 
-export default App 
+export default App

8b24986cbf404e82cb8a32e5b349b2561db8f0e5
DebFiore <deb@mergemedia.ai>
2025-11-06 13:26:36 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Pages from '@/pages/index.jsx'
+import Pages from '@/pages/Index.jsx'
 import { Toaster } from '@/components/ui/toaster'
-import { SchemaManager } from './components/Schema'
 
 function App() {
   return (
     <>
-      <SchemaManager pageType="homepage" />
       <Pages />
       <Toaster />
     </>

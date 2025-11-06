3a8e50111cf1b882b06383f72526849708d0e256
DebFiore <deb@mergemedia.ai>
2025-11-06 13:24:56 -0700

    Delete src/components/Schema directory

--- src/components/Schema/index.js
+++
@@ -1,9 +0,0 @@
-// Base components
-export { default as SchemaBase } from './SchemaBase';
-export { default as SchemaManager } from './SchemaManager';
-
-// Core schemas
-export { default as OrganizationSchema } from './OrganizationSchema';
-export { default as WebSiteSchema } from './WebSiteSchema';
-
-// More schemas will be added here as we create them
